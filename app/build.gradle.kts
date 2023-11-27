b3de089a4313bf7ffb1130595d99306a83a2c61d
nqk09 <nqk090203@gmail.com>
2023-11-28 05:26:12 +0700

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,4 +38,12 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation("jp.wasabeef:recyclerview-animators:4.0.2")
+    implementation("androidx.palette:palette:1.0.0")
+    implementation("io.github.gautanchibde:audiovisualizer:2.2.5")
+    implementation("com.google.android.exoplayer:exoplayer:2.19.1")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.github.jgabrielfreitas:BlurImageView:1.0.1")
+    implementation ("com.github.jitpack:android-example:1.0.1")
+
 }
+
